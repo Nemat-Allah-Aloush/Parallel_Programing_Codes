cea0d6cea0bb414d91f31d73c479de6e6f6a186f
Nemat Allah ALoush <neemat77@gmail.com>
2021-12-15 13:32:14 +0300

    Assignement_0_Finished

--- OpenMP_Assignement_0.cpp
+++ OpenMP_Assignement_0.cpp
@@ -5,9 +5,10 @@
 
 int main(int argc, char *argv[]) {
   int num_words = 0;
+  int i=0;
 // Parallel loop to check each char of the passed string  
 #pragma omp parallel for
-  for (int i = 0; i <= strlen(argv[1]); i++) {
+  for (i = 0; i <= strlen(argv[1]); i++) { 
 #pragma omp atomic update
 // In an atomic block, the number of words is increased by one,
 // if the current char is a space and it is not followed by a space
